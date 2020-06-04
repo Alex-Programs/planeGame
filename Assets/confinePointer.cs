8857d9f79c87b8048fa305e03cd66099a12c0d7a
AlexAndHisScripts <alexcj2802@gmail.com>
2020-06-04 10:34:07 +0100

    Initial commit, basic flight

---
+++ Assets/confinePointer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class confinePointer : MonoBehaviour
+{
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.visible = false;
+    }
+}
